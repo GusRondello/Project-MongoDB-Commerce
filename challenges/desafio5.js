b7fa0670489b2256488bf0435a4792bf0ef76f46
GusRond <gustavorondello99@outlook.com>
2022-10-17 15:20:17 -0300

    fix: indentation

--- challenges/desafio5.js
+++ challenges/desafio5.js
@@ -1,2 +1,8 @@
 db.produtos
-.find({ $or: [{ vendidos: 85 }, { curtidas: 36 }] }, { _id: 0, nome: 1, vendidos: 1, curtidas: 1 });
+.find({ $or: [
+        { vendidos: 85 },
+        { curtidas: 36 }] },
+    { _id: 0,
+        nome: 1,
+        vendidos: 1,
+        curtidas: 1 });
