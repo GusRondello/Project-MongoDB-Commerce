4bf9c91b1dc44168503eea441e193f0cef75e367
GusRond <gustavorondello99@outlook.com>
2022-10-18 11:37:31 -0300

    fix: desafio9.js

--- challenges/desafio9.js
+++ challenges/desafio9.js
@@ -1,5 +1,3 @@
-// db.produtos.find({ "valoresNutricionais.0.quantidade": { $lt: 500 } }, { _id: 0, nome: 1 });
-
 db.produtos.find({ 
     valoresNutricionais: { 
         $elemMatch: { tipo: "calorias", quantidade: { $lt: 500 } } } }, { _id: 0, nome: 1 });
